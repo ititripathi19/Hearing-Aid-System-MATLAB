c53f577fff7d7f7bbbcfcd45071b3f281766ed9d
ititripathi19 <tripathi.i16@iiits.in>
2017-12-08 18:51:08 +0530

    Add files via upload
    "demo.m" is the main file which calls all other functions

---
+++ demo.m
@@ -0,0 +1,8 @@
+clear all; close all; clc;
+input = 'sample.wav';
+g = 50;
+transitionV = [1000, 1500, 2550, 5000];
+Psat=100;
+y = hearingAidF(input,g,Psat,transitionV,newfile);
+snr = abs(snr(y));
+disp(snr);
